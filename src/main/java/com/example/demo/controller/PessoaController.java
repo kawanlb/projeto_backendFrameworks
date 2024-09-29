fd4b27fdf07a5b62f5859d85e76050711e3e3ece
Kawan Santos <kawandev7@gmail.com>
2024-09-29 20:44:45 -0300

    PessoaController - Update

--- src/main/java/com/example/demo/controller/PessoaController.java
+++ src/main/java/com/example/demo/controller/PessoaController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.dto.PessoaDto;
 import com.example.demo.model.Pessoa;
 import com.example.demo.repository.PessoaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,18 +22,16 @@ public class PessoaController {
     @PostMapping
     public ResponseEntity<Pessoa> criarPessoa(@RequestBody PessoaDto pessoaDTO) {
         Pessoa pessoa = new Pessoa();
-        pessoa.setName(pessoaDTO.getNome());
-        pessoa.setEmail(pessoaDTO.getEmail());
-        pessoa.setTelefone(pessoaDTO.getTelefone());
+        BeanUtils.copyProperties(pessoaDTO, pessoa);
 
         pessoa = pessoaRepository.save(pessoa);
-        return ResponseEntity.ok(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Pessoa> buscarPessoaPorId(@PathVariable UUID id) {
         Optional<Pessoa> pessoa = pessoaRepository.findById(id);
         return pessoa.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
     }
 }
